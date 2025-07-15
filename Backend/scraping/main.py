92772e71b5a89a9ce4d30e8dc9a0ab647e25d609
Krishnan Narayanan <krishnannarayanan05@gmail.com>
2025-07-15 19:49:02 +0530

    Forgot requirments.txt file

--- Backend/scraping/main.py
+++ Backend/scraping/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 import supabase_connector
-import uvicorn
 
 app = FastAPI()
 
@@ -30,6 +29,3 @@ async def get_details(info:str):
 @app.get("/")
 async def root():
     return {"message":"Hello World"}
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
